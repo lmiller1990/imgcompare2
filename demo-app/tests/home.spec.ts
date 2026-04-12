dbc6b587db0fdd91613a50595a33b4697c3e213e
Lachlan Miller <lachlan@lachlan-miller.me>
2026-04-12 19:13:12 +1000

    screenshots

--- demo-app/tests/home.spec.ts
+++ demo-app/tests/home.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has content', async ({ page }) => {
   await page.goto('/');
 
+  await page.screenshot({ path: 'homepage.png' });
+
   await expect(page.getByText('Hello world')).toBeVisible();
 });
