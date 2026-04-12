e76b5f6032299fcba5675fb8a28c8434e80466bb
Lachlan Miller <lachlan@lachlan-miller.me>
2026-04-12 14:12:49 +1000

    init

---
+++ packages/cli/index.ts
@@ -0,0 +1,9 @@
+// export
+import { $ } from 'execa'
+
+export async function run() {
+    console.log(process)
+
+    const {} = $``
+
+}
---
+++ packages/cli/test/e2e.spec.ts
@@ -0,0 +1,5 @@
+import { test } from 'vitest'
+
+test("trying it out", () => {
+
+})
