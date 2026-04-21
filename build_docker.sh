0a7a23dbd4d9ab61e5a815cca18478f330d53bb8
Lachlan Miller <lachlan@lachlan-miller.me>
2026-04-21 21:54:13 +1000

    update build for amd64

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
-docker buildx build --load -t lachlanmillerdev/imgcompare-frontend -f ./packages/app/Dockerfile .
-docker buildx build --load -t lachlanmillerdev/imgcompare-server -f ./packages/server/Dockerfile .
+docker buildx build --load --platform linux/amd64 -t lachlanmillerdev/imgcompare-frontend -f ./packages/app/Dockerfile .
+docker buildx build --load --platform linux/amd64 -t lachlanmillerdev/imgcompare-server -f ./packages/server/Dockerfile .
