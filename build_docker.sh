f6d1eb9b9f4dfc770c778638200a0a52c8c6af26
Lachlan Miller <lachlan@lachlan-miller.me>
2026-04-25 09:56:21 +1000

    update docker scripts

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,5 @@
 docker buildx build --load --platform linux/amd64 -t lachlanmillerdev/imgcompare-frontend -f ./packages/app/Dockerfile .
 docker buildx build --load --platform linux/amd64 -t lachlanmillerdev/imgcompare-server -f ./packages/server/Dockerfile .
+
+docker push lachlanmillerdev/imgcompare-frontend
+docker push lachlanmillerdev/imgcompare-server
